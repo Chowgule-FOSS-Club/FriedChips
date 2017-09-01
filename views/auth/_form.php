f650591740b073599358713dfc116070b1001798
Castor Godinho <castorgodinho@yahoo.in>
2017-09-01 10:31:39 +0530

    local commit

--- views/auth/_form.php
+++ views/auth/_form.php
@@ -15,7 +15,7 @@ use yii\widgets\ActiveForm;
         'enableAjaxValidation' => true
     ]); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true] ) ?>
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
