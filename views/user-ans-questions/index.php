c3d00e5a9f30191131470563d8b52966b2682ca5
amnag090 <amnag090@gmail.com>
2017-09-14 00:34:08 +0530

    card view done

--- views/user-ans-questions/index.php
+++ views/user-ans-questions/index.php
@@ -24,39 +24,73 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Create User Ans Questions', ['view'], ['class' => 'btn btn-success']) ;
+        $query = new Query();
+        $query->select(['product.name As pname','users.userid As uid','product.pid As product','users.email As email','users.fname As fname','users.lname As lname','user_ans_questions.created_time as date'])
+        ->from('user_ans_questions' )
+        ->join('INNER JOIN', 'questions','user_ans_questions.qid =questions.qid')
+        ->join('INNER JOIN','product','user_ans_questions.pid =product.pid')
+        ->join('INNER JOIN','users','user_ans_questions.uid =users.userid')
+        ->where('isRead!=true')
+        ->orderBy('user_ans_questions.created_time')
+        ->groupBy('user_ans_questions.created_time');
+
+                $command=$query->createCommand();
+                //echo $command->getRawSql();
+                $data=$command->queryAll();
+                $result=array_values($data);
+
         
+        
+
+
+
       
         ?>
     </p>
 
 
     <div class="wrapper">
-        <div class="cards">
-                <?
-                $query = new Query();
-                $query->select('uid')
-                        ->from('user_ans_questions')
-                        ->all();
+    <?
+                        foreach($result as $r){
 
+                            $d= new DateTime($r['date']);
+                            $date=$d->format('d/m/Y');
+                           //$time = date("Y/m/d h:i:s",$d->format('y-m-d H:i:s'));
+                            $p= $r['product'];
 
-                        $command=$query->createCommand();
-                        echo $command->getRawSql();
-                        $data=$command->queryAll();
-                        foreach($data as $d) echo $d->uid;
-                
+                            $query1= new Query();
+                            $query1->select(['questions.name As qname','user_ans_questions.answer As answer','user_ans_questions.created_time As time'])
+                            ->from('user_ans_questions' )
+                            ->join('INNER JOIN', 'questions','user_ans_questions.qid =questions.qid')
+                            
+                            ->where('isRead!=true')
+                           ->andwhere('pid='.$p)
+                            ->andwhere('uid='.$r['uid']);
+                            
+
+                            $command1=$query1->createCommand();
+                            //echo $command1->getRawSql();
+                            $data1=$command1->queryAll();
+                            $questions=array_values($data1);
+
+                           
+                            
+
                 ?>
+        <div class="cards">
+                
             <div class=" card [ is-collapsed ] ">
                 <div class="card__inner [ js-expander ]">
                     <div class="container-fluid">
                         <div class="row text-center">
                             <div class="col-md-4">
-                                <h3>Product: <small>Bull-dozers</small></h3>
+                                <h3>Product: <small><?= $r['pname'];?></small></h3>
                             </div>
                             <div class="col-md-4">
-                                <h3>User Name: <small>Bull-dozers</small></h3>
+                                <h3>User Name: <small><?= $r['fname']." ".$r['lname'];?></small></h3>
                             </div>
                             <div class="col-md-4">
-                                <h3> <small>dd/mm/yyyy</small></h3>
+                                <h3> <small><?= $date;?></small></h3>
 
                             </div>
 
@@ -69,24 +103,26 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="row  text-center">
                         <div class="col-sm-6 row1">
                             <div class="row ">
-                                <div class="col-xs-12">UserName</div>
+                                <div class="col-xs-12"><?= $r['fname']." ".$r['lname'];?></div>
                             </div>
                             <div class="row">
-                                <div class="col-xs-12">Product Name</div>
+                                <div class="col-xs-12"><?= $r['pname'];?></div>
                             </div>
                             <div class="row">
-                                <div class="col-xs-12">rishikam97@gmail.com</div>
+                                <div class="col-xs-12"><?= $r['email'];?></div>
                             </div>
                         </div>
                     
                         <div class="col-sm-6">
                             <h2>Questions and Answers</h2>
-                            <div class="ques">Q1 What is his top speed ? </div>
-                            <div class="ans"> 90</div>
-                            <div class="ques">Q2 What is his shot speed ? </div>
-                            <div class="ans"> 150</div>
-                            <div class="ques">Q3 Is he fit to play 90mins?</div>
-                            <div class="ans">34</div>
+                            <?  
+                            
+                                foreach($questions as $q){
+                                if($q['time']===$r['date']) {
+                           ?>
+                            <div class="ques"><?= "Q:".$q['qname']?></div>
+                            <div class="ans"><?= "A:".$q['answer']?></div><br>
+                            <?}}?>
                         </div>
 
                     </div>
@@ -95,6 +131,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
             </div>
         </div>
+        <?}?>
     </div>
 
     </div>
