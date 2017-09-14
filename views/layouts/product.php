cdeab9d7c7988ded8e87ca69b83e6ca5b6fce6b6
rishi3095 <rishi3095@github.com>
2017-09-14 09:39:00 +0530

    minor navbar changes

--- views/layouts/product.php
+++ views/layouts/product.php
@@ -43,7 +43,7 @@ ProductAsset::register($this);
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-                <a class="navbar-brand" href="index.php">SALGAOCAR ENGENEERING</a>
+                <a class="navbar-brand" href="index.php">SALGAOCAR ENGENEERS</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
