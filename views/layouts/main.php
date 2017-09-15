a452eb8e2c2b2a31baaf08914d93915790830e08
wenhawk <wendhamgray@gmail.com>
2017-09-15 09:39:29 +0530

    fix for same entry in database

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,10 +11,8 @@
  use yii\helpers\Html;
  use yii\helpers\Json;
  use app\models\Questions;
- use app\assets\UserAnsQuestionsAsset;
  use yii\db\Query;
  date_default_timezone_set('Asia/Calcutta');
- UserAnsQuestionsAsset::register($this);
 
 $bundle = yiister\gentelella\assets\Asset::register($this);
 
@@ -27,15 +25,6 @@ $bundle = yiister\gentelella\assets\Asset::register($this);
     <meta charset="<?= Yii::$app->charset ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-     <!-- Latest compiled and minified CSS -->
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link href='https://fonts.googleapis.com/css?family=Khula' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Cambay' rel='stylesheet'>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
