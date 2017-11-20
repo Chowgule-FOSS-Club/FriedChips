89e23699c8b3aea16a55098e60ef7525ac0ea1f1
wenhawk <wendhamgray@gmail.com>
2017-11-20 10:25:30 +0530

    hit refresh

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=FriedChipsDB',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
